f435f72614b2b9ecc3802c1263544bd65ccee1c3
black-desk <me@black-desk.cn>
2023-02-19 03:22:27 +0800

    feat: config ld.so

--- build.sh
+++ build.sh
@@ -15,3 +15,13 @@ cp deepin.gpg /apricot-rootfs/etc/apt/trusted.gpg.d
 multistrap -f apricot.multistrap
 
 cp sources.list /apricot-rootfs/etc/apt/sources.list
+
+echo 'export PATH="$PATH:/run/host/usr/local/bin:/run/host/usr/bin:/run/host/bin"' \
+	>/apricot-rootfs/etc/profile.d/00-host-path.sh
+
+{
+	echo '/run/host/lib/x86_64-linux-gnu'
+	echo '/run/host/usr/lib/x86_64-linux-gnu'
+	echo '/run/host/lib'
+	echo '/run/host/usr/lib'
+} >>/etc/ld.so.conf.d/host-libs.conf
